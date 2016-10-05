17c65a29a92feacf7fd41c44619f2cb22b50d60d
annoying-bastard <annoying.bastard@gmx.de>
2016-10-06 01:09:25 +0200

    implemented winstreak counter, reworked result logic and win/loss feedback texts

--- rps.js
+++ rps.js
@@ -28,32 +28,32 @@ var generateComputerChoice = function(){
 var compare = function(choice1, choice2){
 	var result = null;
 	if(choice1 === choice2){
-		result = 'The result is a tie!';
+		result = 2;
 	} else if (choice1 === "rock"){
 		if (choice2 === "scissors"){
-			result = 'Rock wins!'
+			result = 0;
 		} else {
-			result = 'Paper wins!'
+			result = 1;
 		}
 	} else if (choice1 === "paper"){
 		if (choice2 === "rock"){
-			result = 'Paper wins!'
+			result = 0;
 		} else {
-			result = 'Scissors win!'
+			result = 1;
 		}
 	} else if (choice1 === "scissors"){
 		if (choice2 === "paper"){
-			result = 'Scissors win!'
+			result = 0;
 		} else { 
-			result = 'Paper wins!'
+			result = 1;
 		}
 	}
 	return result;
 }
 
 
-
-
+var computerStreak = 0;
+var playerStreak = 0;
 
 
 const server = http.createServer((req, res) => {
@@ -64,9 +64,35 @@ const server = http.createServer((req, res) => {
  
   	if(userChoice != undefined){	 	
 	 	var computerChoice = generateComputerChoice();
-	 	console.log(computerChoice, userChoice);
+	 	//console.log(computerChoice, userChoice);
 	 	var result = compare(userChoice, computerChoice);
-	 	var finalHtmlResult = htmlResult.replace('{{result}}', result);
+	 	
+	 	var text = null;
+
+	 	if (result === 1){
+	 		text = 'Computer wins with their ' + computerChoice + ' against your ' +userChoice + '!';
+	 		computerStreak++;
+	 		playerStreak = 0;		
+	 	} else if(result === 0){
+	 		text = 'You reck the computer with your epic ' + userChoice + ' against their puny ' + computerChoice + '!' ; 
+	 		computerStreak = 0;
+	 		playerStreak++;
+	 	} else {
+	 		text = 'It\'s a tie! Game is hard...';
+	 		computerStreak = 0;
+	 		playerStreak = 0;
+	 	}
+	 	var streakText = '';
+	 	if (playerStreak > 0) {
+	 		streakText = 'You have a streak of ' + playerStreak +' wins!'
+	 	} else if (computerStreak > 0){
+	 		streakText = 'You\'re being dominated by the comput0rs streak of ' + computerStreak + ' wins!'
+	 	}
+
+	 	var finalHtmlResult = htmlResult
+	 		.replace('{{result}}', text)
+	 		.replace('{{streak}}', streakText);
+
 	 	res.end(finalHtmlResult);
 	}else{
 		res.end(html);
