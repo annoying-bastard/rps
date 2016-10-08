70b2808088c3b67b0b7a2e3b05ab8d0b711505c2
annoying-bastard <annoying.bastard@gmx.de>
2016-10-08 05:27:17 +0200

    did horrible, HORRIBLE things in a sad attempt of swebian localization. still commiting. so tired...

--- rps.js
+++ rps.js
@@ -9,11 +9,12 @@ const port = 3000;
 const hostname = 'localhost';
 const savedGameMode = '';
 const app = express();
+
 app.use(cookieParser());
 app.use(startSession());
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({defaultLayout: 'mainenEN'}));
 app.set('view engine', 'handlebars');
-
+console.log(exphbs);
 
 
 
@@ -35,6 +36,19 @@ var userInterface = {
 	 	}
 	},
 
+	getLanguage: function(queryResult, req, res) {
+		var lang = req.cookies.lang;
+		if (lang != undefined) {
+			exphbs.defaultLayout = 'main' + lang;
+			return lang;
+		}
+		if (queryResult === 'deSWG' || queryResult === 'enEN') {
+			exphbs.defaultLayout = 'main' + queryResult;
+			res.cookie('lang', queryResult);
+	 		return queryResult;
+	 	}
+	},
+
 	getGameMode: function(queryResult, req, res) {
 		if (req.cookies.gamemode === 'bestoffive' || req.cookies.gamemode === 'bestofthree') {
 			return req.cookies.gamemode;
@@ -52,26 +66,38 @@ app.get('/', (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
 
+
 	const queryResult = req.query.choice;
+	const lang = userInterface.getLanguage(queryResult, req, res);
 	const gameMode = userInterface.getGameMode(queryResult, req, res);
 	const userChoice = userInterface.getUserChoice(queryResult);
-	console.log(req.session);
+	console.log(req.cookies.lang, lang);
 
-  if(userChoice != undefined){
+	if (!lang){
+		res.render('language');
+	} else if(userChoice != undefined){
 	 	const computerChoice = gameLogic.generateComputerChoice();
 	 	var result = gameLogic.compare(computerChoice, userChoice);
 	 	var text = gameLogic.resultTextGenerator(result, computerChoice, userChoice);
-
 		var streakText = gameLogic.streakTextGenerator(result, req.session);
-	 	res.render('result', {result: text, streak: streakText});
 
+		if (lang === 'enEN') {
+			res.render('result', {result: text, streak: streakText});
+		} else if (lang === 'deSWG', {layout: 'maindeSWG'}) {
+			res.render('resultSWG', {result: text, streak: streakText});
+		}
 	}else if (gameMode != undefined){
-
-		res.render('choice');
-		userChoice = url.parse(req.url, true).query.choice;
-
+		if (lang === 'enEN') {
+			res.render('choice');
+		} else if (lang === 'deSWG') {
+			res.render('choiceSWG', {layout: 'maindeSWG'});
+		}
 	} else if (gameMode === undefined) {
-		res.render('gamemode');
+		if (lang === 'enEN') {
+			res.render('gamemode');
+		} else if (lang === 'deSWG') {
+			res.render('gamemodeSWG', {layout: 'maindeSWG'});
+		}
 
 	}
 
